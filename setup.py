615b3155b618ea10ca31a42857c9d3a6e3734956
Robert Niederreiter <office@squarewave.at>
2022-03-21 09:00:45 +0100

    Classifiers

--- setup.py
+++ setup.py
@@ -23,10 +23,10 @@ setup(
     description=shortdesc,
     long_description=longdesc,
     classifiers=[
+        'Development Status :: 5 - Production/Stable',
         'License :: OSI Approved :: BSD License',
         'Intended Audience :: Developers',
         'Topic :: Software Development',
-        'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.7',
