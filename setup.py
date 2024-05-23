faed1e40636239aa151d438acd15e35b4142e8c2
Robert Niederreiter <rnix@squarewave.at>
2024-05-23 15:59:36 +0200

    Back to development: 0.8.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read_file(name):
         return f.read()
 
 
-version = '0.8.1'
+version = '0.8.2.dev0'
 shortdesc = 'Filesystem directory abstraction based on nodes'
 longdesc = '\n\n'.join([read_file(name) for name in [
     'README.rst',
